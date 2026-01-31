6a05375b8471e1c76ae965c2c66aa3941e7df2ae
Hamza-Ahmad1505 <hamziii1505@gmail.com>
2026-01-31 16:19:38 +0500

    Add loops in python with examples implementation

---
+++ 04_loops/03_tea_order.py
@@ -0,0 +1,4 @@
+orders = ["hitesh", "Aman", "Becky", "Carlos"]
+
+for name in orders:
+    print(f"Orders ready for {name}")
---
+++ 04_loops/09_walrus.py
@@ -0,0 +1,30 @@
+# value = 13
+# remainder = value % 5
+
+# if remainder:
+#     print(f"Not Divisible, remainder is {remainder}")
+
+
+value = 13
+
+if remainder := value % 5:
+    print(f"Not divisible, remainder is {remainder}")
+
+
+# available_sizes = ["small", "medium", "large"]
+
+# if (requested_size := input("Enter your chai cup size: ")) in available_sizes:
+#     print(f"Serving {requested_size}")
+# else:
+#     print(f"Size is unavailable - {requested_size}")
+
+
+
+flavours = ["masala", "ginger", "lemon", "mint"]
+
+print("Available flavours: ",flavours)
+
+while (flavour := input("Choose your flavour: ")) not in flavours:
+    print(f"Sorry, {flavour} is not available")
+
+print(f"You choose {flavour} chai")
