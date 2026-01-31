c7483d8b733b933c5eec0c09dd47c0ab0967d3b3
Hamza-Ahmad1505 <hamziii1505@gmail.com>
2026-01-31 16:11:44 +0500

    datatype in python practice

---
+++ 02_datatypes/chapter_3.py
@@ -0,0 +1,32 @@
+# integer
+
+black_tea_grams = 14
+ginger_grams = 3
+
+total_grams = black_tea_grams + ginger_grams
+print(f"Total grams of base tea is {total_grams}g")
+
+remaining_tea = black_tea_grams - ginger_grams
+print(f"Total grams of remaining tea is {remaining_tea}g")
+
+milk_liters = 7
+servings = 4
+milk_per_serving = milk_liters / servings
+print(f"Milk per serving is {milk_per_serving}L")
+
+total_tea_bags = 7
+pots = 4
+bags_per_pot = total_tea_bags // pots
+print(f"While tea bags per pot is {bags_per_pot} bags")
+
+total_candmom_pods = 10
+pods_per_cup = 3
+leftover_pods = total_candmom_pods % pods_per_cup
+print(f"Leftover cardamom pods {leftover_pods}" )
+
+base_flavor_strength = 2
+sclae_factors = 3
+powerful_flavor = base_flavor_strength ** sclae_factors
+
+print(f"scaled flavor strength is {powerful_flavor}")
+
