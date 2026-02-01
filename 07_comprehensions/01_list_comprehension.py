84bfdc19b6153e472473787a4e02f1ebbf6e0007
Hamza-Ahmad1505 <hamziii1505@gmail.com>
2026-02-01 23:49:21 +0500

    Python comprehension Added

---
+++ 07_comprehensions/01_list_comprehension.py
@@ -0,0 +1,11 @@
+menu = [
+    "Masala Chia",
+    "Iced Lemon Tea",
+    "Green Tea",
+    "Iced Peach Tea",
+    "Ginger Tea"
+]
+
+iced_tea = [my_tea for my_tea in menu if "Iced" in my_tea]
+
+print(iced_tea)
