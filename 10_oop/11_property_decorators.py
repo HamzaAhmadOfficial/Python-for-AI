8eeb729503a53ca956d0f2c2df4362eeb34ee0fe
Hamza-Ahmad1505 <hamziii1505@gmail.com>
2026-02-04 23:37:02 +0500

    property decorator added

---
+++ 10_oop/11_property_decorators.py
@@ -0,0 +1,21 @@
+class TeaLeaf:
+    def __init__(self, age):
+        self._age = age  # Private attribute
+
+
+    @property
+    def age(self):
+        return self._age + 2
+    
+    @age.setter
+    def age(self, age):
+        if 1 <= age <= 5:
+            self._age = age
+        else:
+            raise ValueError("Age must be between 1 and 5")
+        
+leaf = TeaLeaf(2)
+print(leaf.age)  # Output: 4
+leaf.age = 4
+print(leaf.age)  # Output: 6
+leaf.age = 6  # Raises ValueError: Age must be between 1 and 5
